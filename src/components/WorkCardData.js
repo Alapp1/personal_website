f1a2dac9a9649ef7ebfc3a8fac77ee07837474b3
adlapp <adlapp@ncsu.edu>
2024-11-01 17:16:35 -0400

    added new project

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,5 +1,6 @@
 import pro2 from "../assets/blank_space.jpeg"
 import pro3 from "../assets/plane2.jpg"
+import thermostat from "../assets/thermostat.jpg"
 
 const ProjectCardData = [
     {
@@ -10,10 +11,11 @@ const ProjectCardData = [
         source: "https://github.com/Alapp1/flight_finder/blob/main/flights.py"
     },
     {
-        imgsrc: pro2,
-        title: "Coming Soon!",
+        imgsrc: thermostat,
+        title: "IoT Thermostat Dashboard",
         text: "Nothing here yet...",
-        view: "https://imgs.search.brave.com/0hYlp5jENb43PH-x2pNLqWZRc_zKZULKTe70otI9qb8/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9lMC5w/eGZ1ZWwuY29tL3dh/bGxwYXBlcnMvMjg0/LzgzMS9kZXNrdG9w/LXdhbGxwYXBlci1i/YWNrZ3JvdW5kLXRl/eHR1cmUtdGV4dHVy/ZXMtZ3JleS1tb25v/Y2hyb21hdGljLXBs/YWluLXNvbGlkLXRo/dW1ibmFpbC5qcGc"
+        view: "https://github.com/Alapp1/thermostat",
+        source: "https://github.com/Alapp1/thermostat"
     },
     {
         imgsrc: pro2,
