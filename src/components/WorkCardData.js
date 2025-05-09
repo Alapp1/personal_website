07e96c40553a0a89af3cfda2ef2a71dce611d963
adlapp <adlapp@ncsu.edu>
2025-05-09 13:01:30 -0400

    Added CTF Repo

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,4 +1,4 @@
-import pro2 from "../assets/blank_space.jpeg"
+import pro2 from "../assets/CTF.jpg"
 import pro3 from "../assets/plane2.jpg"
 import thermostat from "../assets/thermostat.jpg"
 
@@ -19,9 +19,10 @@ const ProjectCardData = [
     },
     {
         imgsrc: pro2,
-        title: "Coming Soon!",
-        text: "Nothing here yet...",
-        view: "https://imgs.search.brave.com/0hYlp5jENb43PH-x2pNLqWZRc_zKZULKTe70otI9qb8/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9lMC5w/eGZ1ZWwuY29tL3dh/bGxwYXBlcnMvMjg0/LzgzMS9kZXNrdG9w/LXdhbGxwYXBlci1i/YWNrZ3JvdW5kLXRl/eHR1cmUtdGV4dHVy/ZXMtZ3JleS1tb25v/Y2hyb21hdGljLXBs/YWluLXNvbGlkLXRo/dW1ibmFpbC5qcGc"
+        title: "CTF Writeups",
+        text: "A collection of CTF challenges collaboratively completed by students",
+        view: "https://github.com/Alapp1/CTF-Writeups",
+        source: "https://github.com/Alapp1/CTF-Writeups"
     }
 ];
 
