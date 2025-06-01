0d2c89d97965d49c01ebc904c4f1cebd2898da50
Alapp1 <lappaiden@gmail.com>
2025-05-31 20:38:21 -0400

    Updated Coursework

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,43 +1,39 @@
 import "./AboutContentStyles.css";
-import React from 'react';
-import {Link} from "react-router-dom";
+import React from "react";
 import Java from "../assets/Java1.jpg";
 import CPic from "../assets/CPic.png";
 
-
 const AboutContent = () => {
   return (
-    <div className = "about">
-        <div className = "left">
-            <h1>Skills/Relevant CourseWork</h1>
-            <ul>
-                <li>Java</li>
-                <li>C</li>
-                <li>Python</li>
-                <li>Operating Systems</li>
-                <li>Data Structures and Algorithms</li>
-            </ul>
-            <Link to="/contact">
-                <button className = "btn-light">Contact</button>
-            </Link>
+    <div className="about">
+      <div className="left">
+        <h1>Languages</h1>
+        <ul>
+          <li>Python</li>
+          <li>C/C++</li>
+          <li>x86 Assembly</li>
+          <li>Java</li>
+        </ul>
+        <h1>CourseWork</h1>
+        <ul>
+          <li>Computer Security</li>
+          <li>Internet Protocols</li>
+          <li>Cryptography</li>
+          <li>Operating Systems</li>
+        </ul>
+      </div>
+      <div className="right">
+        <div className="img-container">
+          <div className="img-stack top">
+            <img src={Java} className="img" alt="img" />
+          </div>
+          <div className="img-stack bottom">
+            <img src={CPic} className="img" alt="img" />
+          </div>
         </div>
-        <div className = "right">
-            <div className = "img-container">
-                <div className="img-stack top">
-                    <img src={Java}
-                    className="img"
-                    alt="img"/>
-                </div>
-                <div className="img-stack bottom">
-                    <img src={CPic}
-                    className="img"
-                    alt="img"/>
-                </div>
-            </div>
-
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutContent
+export default AboutContent;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,41 @@
-import "./FooterStyles.css"
+import "./FooterStyles.css";
 
-import React from 'react'
-import { FaLinkedin, FaMailBulk } from "react-icons/fa"
+import React from "react";
+import { FaLinkedin, FaMailBulk } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div>className = "footer"  
-        <div className= "footer-container">
-            <div className= "left">
-                <div className="email">
-                   <h4><FaMailBulk size = {20} style={{color: "#fff", marginRight: "2rem" }} />
-                    adlapp@ncsu.edu
-                   </h4>
-                   <h4> <FaLinkedin size = {20} style={{color: "#fff", marginRight: "2rem" }} /></h4>
-                </div>
-            </div>
-            <div className= "right">
-
-              <h4>
-                  About Me
-              </h4>
-              <p>
-                I am an undergraduate computer science student at North Carolina State Univeristy 
-              </p>
-              <div className = "social"> 
-                   
-                
-              </div>
-            </div>
+    <div>
+      className = "footer"
+      <div className="footer-container">
+        <div className="left">
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              adlapp@ncsu.edu
+            </h4>
+            <h4>
+              {" "}
+              <FaLinkedin
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+            </h4>
+          </div>
         </div>
+        <div className="right">
+          <h4>About Me</h4>
+          <p>
+            I am an undergraduate computer science student with a concentration
+            in cybersecurity at North Carolina State Univeristy
+          </p>
+          <div className="social"></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
